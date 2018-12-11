667750c693230dc9b5ca228f2850c5ac6a3b3445
xin.luo.pc <55161376@qq.com>
2018-12-11 19:06:26 +0800

    mgo version

--- consignment-service/handler.go
+++ consignment-service/handler.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"context"
+	"gopkg.in/mgo.v2"
+	"log"
+	"shippy/consignment-service/proto/consignment"
+	vesselProto "shippy/vessel-service/proto/vessel"
+)
+
+type handler struct {
+	session      *mgo.Session
+	vesselClient vesselProto.VesselServiceClient
+}
+
+func (s *handler) CreateConsignment(ctx context.Context, req *go_micro_srv_consignment.Consignment, resp *go_micro_srv_consignment.Response) error {
+	defer s.GetRepo().Close()
+	spec := &vesselProto.Specification{Capacity: int32(len(req.Containers)), MaxWeight: req.Weight}
+
+	response, e := s.vesselClient.FindAvailable(ctx, spec)
+	if e != nil {
+		return e
+	}
+	log.Printf("fount vessel: %s \n", response.Vessel.Name)
+	req.VesselId = response.Vessel.Id
+
+	e = s.GetRepo().Create(req)
+	if e != nil {
+		return e
+	}
+
+	resp.Created = true
+	resp.Consignment = req
+	return nil
+}
+
+func (s *handler) GetConsignments(ctx context.Context, req *go_micro_srv_consignment.GetRequest, resp *go_micro_srv_consignment.Response) error {
+	defer s.GetRepo().Close()
+	consignments, e := s.GetRepo().GetAll()
+
+	if e != nil {
+		return e
+	}
+	resp.Consignments = consignments
+
+	return nil
+}
+
+func (s *handler) GetRepo() Repository {
+	return &ConsignmentRepository{s.session.Clone()}
+}
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,75 +1,33 @@
 package main
 
 import (
-	"context"
 	"github.com/micro/go-micro"
 	"log"
+	"os"
 	pb "shippy/consignment-service/proto/consignment"
 	vesselPb "shippy/vessel-service/proto/vessel"
 )
 
-type IRepository interface {
-	Create(consignment *pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.consignments = append(repo.consignments, consignment)
-	return consignment, nil
-}
-
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-//服务
-
-type service struct {
-	repo IRepository
-	//consignment-service 作为客户端调用 vessel-service的函数
-	vesselClient vesselPb.VesselServiceClient
-}
-
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	//检查是或否有何时的货轮
-
-	spec := &vesselPb.Specification{Capacity: int32(len(req.Containers)), MaxWeight: req.Weight}
-
-	response, e := s.vesselClient.FindAvailable(context.Background(), spec)
-	if e != nil {
-		return e
-	}
-
-	log.Printf("fount vessel: %s \n", response.Vessel.Name)
-	req.VesselId = response.Vessel.Id
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
 func main() {
+	var host string
+	if host = os.Getenv("DB_HOST"); host == "" {
+		host = defaultHost
+	}
 
+	session, e := CreateSession(host)
+	if e != nil {
+		// We're wrapping the error returned from our CreateSession
+		// here to add some context to the error.
+		log.Panicf("Could not connect to datastore with host %s - %v", host, e)
+	}
 	server := micro.NewService(micro.Name("go_micro_srv_consignment"), micro.Version("latest"))
 	server.Init()
-	repo := &Repository{}
-	vesselClient := vesselPb.NewVesselServiceClient("go_micro_srv_vessel",server.Client())
-	pb.RegisterShippingServiceHandler(server.Server(), &service{repo: repo,vesselClient:vesselClient})
+	vesselClient := vesselPb.NewVesselServiceClient("go_micro_srv_vessel", server.Client())
+	pb.RegisterShippingServiceHandler(server.Server(), &handler{session: session, vesselClient: vesselClient})
 
 	if e := server.Run(); e != nil {
 		log.Fatalf("failed to serve: %v", e)
